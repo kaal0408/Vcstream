3db3f985b443490c59e75f34b5a82ccda7e3ed14
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2021-12-01 21:13:18 +0530

    Add files via upload

---
+++ vc/services/queues/__init__.py
@@ -0,0 +1,12 @@
+from Musical.services.queues.queues import clear 
+
+from Musical.services.queues.queues import get
+
+from Musical.services.queues.queues import is_empty
+
+from Musical.services.queues.queues import put
+
+from Musical.services.queues.queues import task_done
+
+__all__ = ["clear", "get", "is_empty", "put", "task_done"]
+
