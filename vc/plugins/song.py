2808a3d403367517da1f4fb6e07cf3fc5bf022f9
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2021-12-01 20:51:30 +0530

    Update song.py

--- vc/plugins/song.py
+++ vc/plugins/song.py
@@ -13,12 +13,10 @@ import aiofiles
 import aiohttp
 import requests
 import wget
-import yt_dlp
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait, MessageNotModified
 from pyrogram.types import Message
 from youtube_search import YoutubeSearch
-from yt_dlp import YoutubeDL
 
 from vc.config import BOT_USERNAME as bn
 from helpers.decorators import humanbytes
