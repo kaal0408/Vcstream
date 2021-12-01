195a3e7345473da09b4e3fc8315fd252cfe07238
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2021-12-01 20:22:29 +0530

    Update play.py

--- vc/plugins/play.py
+++ vc/plugins/play.py
@@ -16,7 +16,6 @@ loop = asyncio.get_event_loop()
 
 @bot.on_message(
     filters.command(["vplay", "vtelegram"]))
-)
 async def stream(client, message):
     reply = message.reply_to_message
     user_str = await user_input(message.text)
