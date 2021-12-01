2f07051e928531b3ef990ca78321d385ebf0d1c6
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2021-12-01 21:14:11 +0530

    Update __init__.py

--- vc/services/queues/__init__.py
+++ vc/services/queues/__init__.py
@@ -1,12 +1,12 @@
-from Musical.services.queues.queues import clear 
+from vc.services.queues.queues import clear 
 
-from Musical.services.queues.queues import get
+from vc.services.queues.queues import get
 
-from Musical.services.queues.queues import is_empty
+from vc.services.queues.queues import is_empty
 
-from Musical.services.queues.queues import put
+from vc.services.queues.queues import put
 
-from Musical.services.queues.queues import task_done
+from vc.services.queues.queues import task_done
 
 __all__ = ["clear", "get", "is_empty", "put", "task_done"]
 
