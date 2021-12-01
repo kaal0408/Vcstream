7efd79d0bf9c042fde2e3e11106863b44917d8b9
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2021-12-01 21:07:02 +0530

    Create __main__.py

---
+++ vc/__main__.py
@@ -0,0 +1,23 @@
+import requests
+from pyrogram import Client as Bot
+from vc.config import API_HASH
+from vc.config import API_ID
+from vc.config import BG_IMAGE
+from vc.config import BOT_TOKEN
+from vc.services.callsmusic import run
+
+response = requests.get(BG_IMAGE)
+file = open("./etc/foreground.png", "wb")
+file.write(response.content)
+file.close()
+
+bot = Bot(
+    ":memory:",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="Musical.modules"),
+)
+
+bot.start()
+run()
